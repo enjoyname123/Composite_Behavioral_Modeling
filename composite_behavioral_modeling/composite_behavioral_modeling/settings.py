4d5a3d987da14c983922e8ccdf1245427a6d4c4f
Dainty Wilder <mamiamman@gmail.com>
2026-04-16 20:53:40 +0530

    Update settings.py

--- composite_behavioral_modeling/composite_behavioral_modeling/settings.py
+++ composite_behavioral_modeling/composite_behavioral_modeling/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Template/media')
 
